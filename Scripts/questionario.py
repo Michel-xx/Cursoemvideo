6d263646a3c38946a0a990ffddbb2e8849c83bec
Michel-xx <pereiramichel433@gmail.com>
2026-05-28 21:20:29 -0300

    correção ortografica

--- Scripts/questionario.py
+++ Scripts/questionario.py
@@ -2,4 +2,4 @@ nome =  ((input("Qual é o seu nome: ")))
 idade = int((input("Qual é a sua idade: ")))
 peso =  float((input("Qual é o seu peso: ")))
 
-print('Olá, meu nome é' , nome , 'e eu tenho' , idade , 'anos, e tabém peso' , peso , 'kg')
+print('Olá, meu nome é' , nome , 'e eu tenho' , idade , 'anos, e também peso' , peso , 'kg')
